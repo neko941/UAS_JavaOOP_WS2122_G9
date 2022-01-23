2e60a346cbd8224357c2d4f388857c9d5c8e0646
klangthang <nguyenphukhang3010@gmail.com>
2022-01-23 03:28:21 +0100

    2022-01-23

--- src/main/java/Controllers/RegistrationController.java
+++ src/main/java/Controllers/RegistrationController.java
@@ -129,4 +129,17 @@ public class RegistrationController {
         Stage stage = (Stage) closeButton.getScene().getWindow();
         stage.close();
     }
+
+    public void SignInButtonOnAction(ActionEvent event) {
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/UI/LoginUI.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            System.err.println(String.format("Error: %s", e.getMessage()));
+        }
+    }
 }
